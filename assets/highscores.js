67caa7ba3716d0f439894c760a2ae8307fd6ff4a
Pholobo <ouyangphoenix@gmail.com>
2023-01-18 21:40:17 -0500

    adds event listeners for back and clear high scores buttons

--- assets/highscores.js
+++ assets/highscores.js
@@ -4,23 +4,22 @@ var highScores = document.getElementById("high-scores");
 
 function displayScores () {
     var scoreList = JSON.parse(localStorage.getItem("scoreList")) || [];
-    console.log(scoreList)
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < scoreList.length ; i++) {
         var li = document.createElement("li");
         li.setAttribute("class", "highScoreList");
         li.textContent = scoreList[i].initials + " - " + scoreList[i].score;
         highScores.appendChild(li);
     }
-}
+};
 
 goBack.addEventListener("click", function() {
    location.href = "index.html"
-})
+});
 
 clear.addEventListener("click", function() {
     localStorage.clear();
     highScores.innerHTML = "";
-})
+});
 
 displayScores();
